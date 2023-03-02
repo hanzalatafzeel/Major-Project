d0b214842df5b6b6b18eca4a24ff4d5d70c8cc7b
FAHAD ANSARI <fahadansari6913@gmail.com>
2023-03-02 16:07:43 +0530

    v

--- checkout.php
+++ checkout.php
@@ -60,7 +60,7 @@ $count = -1;
 
     <!-- Nav Bar -->
     <nav class="navbar navbar-expand-lg navbar-dark">
-        <a class="navbar-brand pageName" href="index.html">Canteen</a>
+        <a class="navbar-brand pageName" href="./">Canteen</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
             aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
